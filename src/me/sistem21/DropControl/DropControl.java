3bbc248837da355e3a999d2e4c121ad9ed67364b
sistem21 <sistem21@users.noreply.github.com>
2015-07-21 21:32:32 +0200

    Update DropControl.java

--- src/me/sistem21/DropControl/DropControl.java
+++ src/me/sistem21/DropControl/DropControl.java
@@ -30,30 +30,19 @@ public class DropControl extends JavaPlugin implements Listener{
 		
 		if(config.getBoolean("DropControl")){
 		
-		
 			if(en instanceof PigZombie){
 			
-			
 				Random r = new Random();
-			
 				double chance = config.getDouble("dropChancePigZombie")/100;
-			
 				int amount = r.nextInt(2);
-			
 				int amount1 = r.nextInt(4);
-			
 				for(int i = 0; i < e.getDrops().size(); i++){
-				
 				
 					if(chance < 50){
 					e.getDrops().set(i, new ItemStack(Material.GOLD_INGOT, amount));
-				
 					}else{
-					
 						e.getDrops().set(i, new ItemStack(Material.GOLD_INGOT, amount1));
-				
 					}
-				
 				}
 			
 			}else if(en instanceof IronGolem){
